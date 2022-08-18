5a619ba8d2514fdb7f0163cb5c4971d21ccf6b16
Abubakr Muhammad <realchampiontheno1@gmail.com>
2022-08-18 12:29:59 +0500

    provider pattern

---
+++ provider.js
@@ -0,0 +1,15 @@
+// PROVIDER PATTERN
+// Make data available to multiple child components
+
+// prop drilling: the case when we pass props far down the component tree
+
+/*
+
+  THE REACT PROVIDER PATTERN
+
+  React.createContext();
+  React.useContext():
+
+  <context.Provider>;
+
+*/
